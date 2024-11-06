b916a07951e2a38266684740db976bc93b386031
summer <wsh6922@naver.com>
2024-11-06 12:00:25 +0900

    서버 시작

---
+++ src/main/java/com/namu/thenamu/ThenamuApplication.java
@@ -0,0 +1,13 @@
+package com.namu.thenamu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThenamuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThenamuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/namu/thenamu/ThenamuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namu.thenamu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThenamuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
